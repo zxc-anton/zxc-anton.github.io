9d442c8e7afb2c3214c73a18334c053243fd7c0f
zxc-anton <antontaradaev8@gmail.com>
2026-03-03 19:54:19 +0700

    new

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div>my new app</div>
+  <h1>You did it!</h1>
+  <p>
+    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
+    documentation
+  </p>
 </template>
 
 <style scoped></style>
